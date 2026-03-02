7a22ca43b8a77abdf7ec894326d334606eb2c24e
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2026-03-02 13:06:02 -0500

    feat: Added sidebar (#4)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,52 +5,56 @@ import './page.scss';
 import { BalanceCard } from '@/features/metrics/BalanceCard';
 import { MetricCard } from '@/features/metrics/MetricCard';
 import { TransactionCard } from '@/features/metrics/TransactionCard';
+import { Sidebar } from '@/features/sidebar/Sidebar';
 import { Button } from '@/shared/components/ui/button';
 import { Calendar } from '@/shared/components/ui/calendar';
-import { Coffee } from 'lucide-react';
 import { DateField } from '@/shared/components/ui/date-field';
 import { DatePicker } from '@/shared/components/ui/date-picker';
 import { NumberField } from '@/shared/components/ui/number-field';
 import { TextField } from '@/shared/components/ui/text-field';
+import { Coffee } from 'lucide-react';
 
 export default function Home() {
   return (
-    <main className="page-container">
-      <h1>finance-app</h1>
-      <Button type={'secondary'} size={'large'} border={true}>
-        Cancel
-      </Button>
-      <Button type={'primary'} size={'large'}>
-        Save Transaction
-      </Button>
-      <BalanceCard
-        balance={'20,589.30'}
-        stats={[
-          { icon: 'income', label: 'Income', value: '+$8,450.00' },
-          { icon: 'expense', label: 'Expenses', value: '-$3,280.50' },
-        ]}
-      ></BalanceCard>
-      <MetricCard
-        title={'Monthly Expenses'}
-        value={'$3,280'}
-        icon={'expense'}
-        trend="12% vs last month"
-      ></MetricCard>
-      <TransactionCard
-        icon={<Coffee size={20}/>}
-        title={'Starbucks Coffee'}
-        description={'Food & Drinks'}
-        value={'-$5.80'}
-        date={'Today'}
-      ></TransactionCard>
-      <TextField label="Name" />
-      <NumberField
-        label="Amount"
-        formatOptions={{ style: 'currency', currency: 'COP' }}
-      />
-      <DateField label="Date" />
-      <Calendar />
-      <DatePicker />
-    </main>
+    <div className="page">
+      <Sidebar></Sidebar>
+      <main className="page-container">
+        <h1>finance-app</h1>
+        <Button type={'secondary'} size={'large'} border={true}>
+          Cancel
+        </Button>
+        <Button type={'primary'} size={'large'}>
+          Save Transaction
+        </Button>
+        <BalanceCard
+          balance={'20,589.30'}
+          stats={[
+            { icon: 'income', label: 'Income', value: '+$8,450.00' },
+            { icon: 'expense', label: 'Expenses', value: '-$3,280.50' },
+          ]}
+        ></BalanceCard>
+        <MetricCard
+          title={'Monthly Expenses'}
+          value={'$3,280'}
+          icon={'expense'}
+          trend="12% vs last month"
+        ></MetricCard>
+        <TransactionCard
+          icon={<Coffee size={20} />}
+          title={'Starbucks Coffee'}
+          description={'Food & Drinks'}
+          value={'-$5.80'}
+          date={'Today'}
+        ></TransactionCard>
+        <TextField label="Name" />
+        <NumberField
+          label="Amount"
+          formatOptions={{ style: 'currency', currency: 'COP' }}
+        />
+        <DateField label="Date" />
+        <Calendar />
+        <DatePicker />
+      </main>
+    </div>
   );
 }
